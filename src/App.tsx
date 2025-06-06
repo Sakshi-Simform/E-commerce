46add48930338f852291ac50d4427c50f3b48338
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-06 11:55:20 +0530

    feature: add main file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import '@/styles/App.css'; 
+
+function App() {
+  return (
+    <div className="w-full min-h-[500px] h-screen bg-gradient-to-t from-[rgb(73,22,73)] to-black font-poppins">
+      <h1 className="text-white">Shopify</h1>
+    </div>
+  );
+}
+
+export default App;
