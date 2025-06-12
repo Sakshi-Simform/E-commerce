a8ccb405231f4ff65f1e90326ab5f4776235a55e
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-12 10:49:49 +0530

    feature: add component for fallback

---
+++ src/components/ErrorFallback/ErrorFallback.tsx
@@ -0,0 +1,3 @@
+export const ErrorFallback = () => {
+  return <h2>Oops! Something went wrong.</h2>;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { createRoot } from 'react-dom/client';
-import {ErrorBoundary} from '@/components/ErrorBoundry/ErrorBoundry.tsx';
+import { ErrorBoundary } from '@/components/ErrorBoundry/ErrorBoundry.tsx';
+import {ErrorFallback} from '@/components/ErrorFallback/ErrorFallback.tsx';
 import App from './App.tsx';
 import '@/App.css';
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary fallback={<h2>Oops something went Wrong !</h2>}>
-      <App />
-    </ErrorBoundary>
+  <ErrorBoundary fallback={<ErrorFallback />}>
+    <App />
+  </ErrorBoundary>
 );
