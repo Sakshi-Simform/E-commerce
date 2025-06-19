40178ca9a70d259a8c0c95e181a868bb081ff85c
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-19 13:36:01 +0530

    feature: add util and main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from '@/components/ErrorBoundry/ErrorBoundry.tsx';
 import { ErrorFallback } from './components/ErrorFallback/ErrorFallback.tsx';
+import { SessionProvider } from './Context/SessionContext.tsx';
 import App from './App.tsx';
 import '@/App.css';
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <ErrorBoundary fallback={<ErrorFallback />}>
-    <App />
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </SessionProvider>
   </ErrorBoundary>
 );
