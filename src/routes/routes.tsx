390daf4c4dedde9af53d6ed9c39b0b6e62b5a007
Sakshi Shah <sakshi.shah@simformsolutions.com>
2025-06-19 13:35:31 +0530

    feature: add routes and types

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,14 @@
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import Layout from "@/Layout/Layout";
 import { HomePage } from "@/pages/HomePage";
-import { ProductDetail } from "@/components/Products/ProductDetail";
 import { NotFound } from "@/components/Notfound/NotFound";
 import { SignIn } from "@/pages/SignIn";
 import { SignUp } from "@/pages/SignUp";
 import PrivateRoute from "@/Auth/PrivateRoutes";
-import type { SupabaseSession } from "@/types/supabase.type";
+import { ProductDetail } from "@/components/Products/ProductDetail";
 
 export const routerWithSession = (
-  session: SupabaseSession | null,
+  session: string | null,
   loading: boolean
 ) =>
   createBrowserRouter([
@@ -37,4 +36,4 @@ export const routerWithSession = (
       path: "*",
       element: <NotFound />,
     },
-  ]);
+]);
